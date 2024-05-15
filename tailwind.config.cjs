0956143fee97f904869330e5a1aa61badf854576
Jeroen Reumkens <hello@jeroenreumkens.nl>
2024-05-15 10:07:14 +0200

    Added starting point

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,6 @@
 export default {
   content: ["./src/**/*.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    backgroundImage: {
-      "primary-gradient": "linear-gradient(to bottom, #B59CF8, #F5F2FF 48%, #B59CF8 68%, #8273A9 100%)",
-      frost: "repeating-linear-gradient(to right, rgba(255,255,255,.04) 0%, rgba(0,0,0,.39) 49%, rgba(255,255,255,.27) 100%)"
-    },
     colors: {
       black: "#000113",
       white: "#fff",
